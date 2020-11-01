920f8fafb4932da41f27d1ee807e3bd22ba023da
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-11-01 01:00:38 +0100

    Initial Commit

---
+++ src/Chapter_19_Generics/Exercise_19_03/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author J�rgen
+ */
+package Chapter_19_Generics.Exercise_19_03;
+
+/**
+ *
+ * @author Jørgen
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Pair<Integer> pair = new Pair(5, 30);
+        Pair<String> pair2 = new Pair("This is a String", "This is another String");
+        System.out.println(pair);
+        System.out.println(pair2);
+    }
+    
+}
